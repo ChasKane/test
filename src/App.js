fe38f785e3d74bf428a621985f54f70544f103d2
YoungSage <LawrenceCKane@gmail.com>
2019-06-10 14:56:33 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HttpsRedirect from 'react-https-redirect';
 
 function App() {
   return (
+      <HttpsRedirect>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,6 +22,8 @@ function App() {
         </a>
       </header>
     </div>
+          <HttpsRedirect>
+
   );
 }
 
